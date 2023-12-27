b9870f8cc83246715e3e0c0103c8424e0b0cd72c
VadimIvanov21 <144548392+VadimIvanov21@users.noreply.github.com>
2023-12-27 10:37:24 +0300

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -65,3 +65,30 @@ for (int i = 0;  s1[i] != '\0'; i++){
 return true;
 }
 
+
+
+bool isLetter(char buk){
+    return ((buk >= 'A' && buk <= 'Z') || (buk >= 'a' && buk <= 'z') || (buk >= 'À' && buk <= 'ß') || (buk >= 'à' && buk <= 'ÿ'));
+}
+
+int itc_countWords(string str) {
+    int a = 0;
+    bool w = false;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] != ' ') {
+            if (!isLetter(str[i]) && str[i] == '#') {
+                return 0;
+            }
+
+            if (!w) {
+                a++;
+                w = true;
+            }
+        } else {
+            w = false;
+        }
+    }
+
+    return a;
+}
